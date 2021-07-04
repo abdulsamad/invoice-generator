afd5b0425bc10704586153f12bce1d7f42486fee
AbdulSamad <Samk8778@yahoo.com>
2021-07-04 17:48:11 +0530

    :hammer: Added typescript d.ts

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,19 @@
-import toggleNav from './toggle-nav';
+import Alpine from 'alpinejs';
 
-toggleNav();
+// Alpine Store (Global State)
+document.addEventListener('alpine:init', () => {
+  Alpine.store('invoiceData', {
+    fields: ['Name', 'Reg No', 'Model No', 'Chasis No', 'Engine No', 'Color'],
+  });
+});
+
+// Start Alpine
+declare global {
+  interface Window {
+    Alpine: any;
+  }
+}
+
+window.Alpine = Alpine;
+
+Alpine.start();
