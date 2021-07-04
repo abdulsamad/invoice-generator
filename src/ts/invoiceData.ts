ed5963f1442e77c1ba03a9ac617f86e17b1471c0
AbdulSamad <Samk8778@yahoo.com>
2021-07-04 20:38:30 +0530

    :construction: Added Invoice data structure

--- src/ts/invoiceData.ts
+++ src/ts/invoiceData.ts
@@ -1,16 +1,41 @@
+interface Ifield {
+  name: string; // Input label
+  type?: string; // Input type
+  placeholder?: string; // Input placeholder
+  required?: boolean; // Input required
+  value?: string; // Input value
+}
+
 interface IDetails {
   fields: {
-    VehicleDetails: string[];
-    CustomerDetails: string[];
-    PaymentDetails: string[];
+    VehicleDetails: Ifield[];
+    CustomerDetails: Ifield[];
+    PaymentDetails: Ifield[];
   };
 }
 
 const details: IDetails = {
   fields: {
-    VehicleDetails: ['Name', 'Reg No', 'Model No', 'Chasis No', 'Engine No', 'Color'],
-    CustomerDetails: ['Name', 'Address', 'LIC No', 'Contact No'],
-    PaymentDetails: ['Mode', 'Vehicle Amount', 'Down Payment', 'Transfer Amount', 'Total Amount'],
+    VehicleDetails: [
+      { name: 'Name', placeholder: 'Enter vehicle name' },
+      { name: 'Reg No', placeholder: 'Enter vehicle reg number' },
+      { name: 'Chasis No', placeholder: 'Enter vehicle chasis number' },
+      { name: 'Engine No', placeholder: 'Enter vehicle engine number' },
+      { name: 'Color', placeholder: 'Enter vehicle color' },
+    ],
+    CustomerDetails: [
+      { name: 'Name', placeholder: 'Enter customer name' },
+      { name: 'Address', placeholder: 'Enter customer address' },
+      { name: 'LIC No', placeholder: 'Enter customer LIC number' },
+      { name: 'Contact No', type: 'number', placeholder: 'Enter customer contact number' },
+    ],
+    PaymentDetails: [
+      { name: 'Mode', placeholder: 'Enter mode of payment', required: true },
+      { name: 'Vehicle Amount', placeholder: 'Enter vehicle amount', required: true },
+      { name: 'Down Payment', placeholder: 'Enter downpayment amount', required: true },
+      { name: 'Transfer Amount', placeholder: 'Enter transfer amount', required: true },
+      { name: 'Total Amount', placeholder: 'Enter total amount', required: true },
+    ],
   },
 };
 
