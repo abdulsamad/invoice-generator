98265e48365ffac82b2415002bed3365771be91c
AbdulSamad <Samk8778@yahoo.com>
2021-07-04 17:43:05 +0530

    :lipstick: Added base nav, form and action buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,13 @@
 import Alpine from 'alpinejs';
 
+// Alpine Store (Global State)
+document.addEventListener('alpine:init', () => {
+  Alpine.store('invoiceData', {
+    fields: ['Name', 'Reg No', 'Model No', 'Chasis No', 'Engine No', 'Color'],
+  });
+});
+
+// Start Alpine
 window.Alpine = Alpine;
 
 Alpine.start();
