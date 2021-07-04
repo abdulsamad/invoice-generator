1f738bf5f84cc322d66362af1d46065ca45ba26d
AbdulSamad <Samk8778@yahoo.com>
2021-07-04 23:19:13 +0530

    :lipstick: Added conditional select elem in fields

--- src/ts/invoiceData.ts
+++ src/ts/invoiceData.ts
@@ -3,7 +3,7 @@ interface Ifield {
   type?: string; // Input type
   placeholder?: string; // Input placeholder
   required?: boolean; // Input required
-  value?: string; // Input value
+  value?: string | string[]; // Input value
 }
 
 interface IDetails {
@@ -17,24 +17,78 @@ interface IDetails {
 const details: IDetails = {
   fields: {
     VehicleDetails: [
-      { name: 'Name', placeholder: 'Enter vehicle name' },
-      { name: 'Reg No', placeholder: 'Enter vehicle reg number' },
-      { name: 'Chasis No', placeholder: 'Enter vehicle chasis number' },
-      { name: 'Engine No', placeholder: 'Enter vehicle engine number' },
-      { name: 'Color', placeholder: 'Enter vehicle color' },
+      {
+        name: 'Name',
+        placeholder: 'Enter vehicle name',
+      },
+      {
+        name: 'Registration Number',
+        placeholder: 'Enter vehicle registration number',
+      },
+      {
+        name: 'Chassis No',
+        placeholder: 'Enter vehicle chassis number',
+      },
+      {
+        name: 'Engine No',
+        placeholder: 'Enter vehicle engine number',
+      },
+      {
+        name: 'Color',
+        placeholder: 'Enter vehicle color',
+      },
     ],
     CustomerDetails: [
-      { name: 'Name', placeholder: 'Enter customer name' },
-      { name: 'Address', placeholder: 'Enter customer address' },
-      { name: 'LIC No', placeholder: 'Enter customer LIC number' },
-      { name: 'Contact No', type: 'number', placeholder: 'Enter customer contact number' },
+      {
+        name: 'Name',
+        placeholder: 'Enter customer name',
+      },
+      {
+        name: 'Address',
+        placeholder: 'Enter customer address',
+      },
+      {
+        name: 'LIC No',
+        placeholder: 'Enter customer LIC number',
+      },
+      {
+        name: 'Contact No',
+        type: 'number',
+        placeholder: 'Enter customer contact number',
+      },
     ],
     PaymentDetails: [
-      { name: 'Mode', placeholder: 'Enter mode of payment', required: true },
-      { name: 'Vehicle Amount', placeholder: 'Enter vehicle amount', required: true },
-      { name: 'Down Payment', placeholder: 'Enter downpayment amount', required: true },
-      { name: 'Transfer Amount', placeholder: 'Enter transfer amount', required: true },
-      { name: 'Total Amount', placeholder: 'Enter total amount', required: true },
+      {
+        name: 'Mode',
+        type: 'select',
+        placeholder: 'Enter mode of payment',
+        required: true,
+        value: ['Cash', 'Cheque', 'Bank Transfer', 'E-Wallet', 'Credit Card', 'Debit Card'],
+      },
+      {
+        name: 'Vehicle Amount',
+        type: 'number',
+        placeholder: 'Enter vehicle amount',
+        required: true,
+      },
+      {
+        name: 'Down Payment',
+        type: 'number',
+        placeholder: 'Enter downpayment amount',
+        required: true,
+      },
+      {
+        name: 'Transfer Amount',
+        type: 'number',
+        placeholder: 'Enter transfer amount',
+        required: true,
+      },
+      {
+        name: 'Total Amount',
+        type: 'number',
+        placeholder: 'Enter total amount',
+        required: true,
+      },
     ],
   },
 };
