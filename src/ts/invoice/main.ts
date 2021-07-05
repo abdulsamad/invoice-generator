8826dc33a880fce7d3e2bc733ced7cec962ceb14
AbdulSamad <Samk8778@yahoo.com>
2021-07-05 20:57:10 +0530

    :lipstick: Added actions toggle

--- src/ts/invoice/main.ts
+++ src/ts/invoice/main.ts
@@ -4,9 +4,21 @@ import invoiceData from './invoiceData';
 
 // Alpine Store (Global State)
 document.addEventListener('alpine:init', () => {
+  // Form fields store
   Alpine.store('invoiceData', invoiceData);
+
+  // Toggle action buttons store
+  Alpine.store('actions', {
+    visible: false,
+
+    toggleActions() {
+      this.visible = true;
+    },
+  });
+
+  // Custom methods store
   Alpine.store('customMethods', {
-    updateInvoiceNumber: (ev: Event) => {
+    updateInvoiceNumber(ev: Event) {
       const elem = ev.target as HTMLInputElement;
       localStorage.setItem('invoiceNumber', elem.value);
     },
