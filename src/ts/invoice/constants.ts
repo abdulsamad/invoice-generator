0a328c149c672625e7821fa27b2ff606e4c650a2
AbdulSamad <Samk8778@yahoo.com>
2021-07-07 13:35:27 +0530

    :construction: Increased font size

--- src/ts/invoice/constants.ts
+++ src/ts/invoice/constants.ts
@@ -10,5 +10,5 @@ export const DOC_WIDTH = 210;
 export const DOC_HEIGHT = 297;
 export const X_Y_MARGIN = 10;
 export const LINE_MAX_WIDTH = DOC_WIDTH - X_Y_MARGIN * 2;
-export const FONT_SIZE = 10; // 10pt
+export const FONT_SIZE = 12; // 12pt
 export const FONT_FAMILY = 'times';
